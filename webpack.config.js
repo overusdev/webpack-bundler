f6917df8f6d8eee2969fc522e08abb86256e57c2
Сергей Вилков <vilkov@reg.ru>
2021-03-20 02:45:09 +0300

    JS bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,24 @@
 const path = require('path');
-const globby = require('globby');
 const glob = require('glob');
 
+function getEntries() {
+    const entries = {};
+
+    glob.sync('./src/**/*.js').forEach( item => {
+        const moduleName = item.replace("./src/", "").replace(".js", "");
+        const pathTo = `${moduleName}/${moduleName}`;
+
+        entries[ pathTo ] = item;
+    } );
+
+    return entries;
+}
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'development',
+    entry: getEntries(),
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'STATIC'),
+    },
+    mode: 'development',
 };
-
-console.log(__dirname);
-
-
-/**
- * 
- *  https://github.com/sindresorhus/globby
- * 
- */
-// (async () => {
-// 	const paths = await globby(['src/**/*.js']);
-// 	console.log(paths);
-// })();
-
-globby(['src/**/*.js',]).then(paths => {
-    console.log(paths);
-});
-
-/**
- * 
- * https://github.com/isaacs/node-glob
- * 
- */
-
-glob("src/**/*.js", function (er, files) {
-    console.log(files);
-});
